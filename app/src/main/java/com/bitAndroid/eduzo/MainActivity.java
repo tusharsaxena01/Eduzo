6769d78b82e52a64749aa4637912e065dd2d62bf
Abhi Saxena <saxena.abhi7007@gmail.com>
2023-07-27 13:34:30 +0530

    added splash screen and get started page

--- app/src/main/java/com/bitAndroid/eduzo/MainActivity.java
+++ app/src/main/java/com/bitAndroid/eduzo/MainActivity.java
@@ -2,17 +2,30 @@ package com.bitAndroid.eduzo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 
 import com.bitAndroid.eduzo.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
+    SharedPreferences sp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        sp = getSharedPreferences("data", 0);
+        binding.btnGetStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int get_started = 1;
+                sp.edit()
+                        .putInt("first_visit", get_started)
+                        .apply();
+            }
+        });
     }
 }
--- app/src/main/java/com/bitAndroid/eduzo/SplashActivity.java
+++ app/src/main/java/com/bitAndroid/eduzo/SplashActivity.java
@@ -3,21 +3,29 @@ package com.bitAndroid.eduzo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
 
+    SharedPreferences sp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        sp = getSharedPreferences("data", 0);
         Intent mainActivity = new Intent(SplashActivity.this, MainActivity.class);
+        Intent welcomeActivity = new Intent(SplashActivity.this, WelcomeActivity.class);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(mainActivity);
+                int first_visit = sp.getInt("first_visit", 0);
+                if(first_visit == 0)
+                    startActivity(mainActivity);
+                else
+                    startActivity(welcomeActivity);
             }
         }, 3000);
     }
