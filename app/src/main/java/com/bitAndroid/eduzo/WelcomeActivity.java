c70d456857e1bbf7684ada33854350483326ef37
Abhi Saxena <saxena.abhi7007@gmail.com>
2023-07-28 00:34:02 +0530

    welcome screen design updated

--- app/src/main/java/com/bitAndroid/eduzo/WelcomeActivity.java
+++ app/src/main/java/com/bitAndroid/eduzo/WelcomeActivity.java
@@ -2,7 +2,9 @@ package com.bitAndroid.eduzo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.bitAndroid.eduzo.databinding.ActivityWelcomeBinding;
 
@@ -14,7 +16,35 @@ public class WelcomeActivity extends AppCompatActivity {
         binding = ActivityWelcomeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        binding.ivBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent getStartedIntent = new Intent(WelcomeActivity.this, MainActivity.class);
+                startActivity(getStartedIntent);
+            }
+        });
 
+        binding.btnGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Todo: add login with google
+            }
+        });
 
+        binding.btnEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Todo: add login with email
+            }
+        });
+
+        binding.tvLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Todo: add intent to move to login page
+//                Intent loginIntent = new Intent(WelcomeActivity.this, LoginActivity.class);
+//                startActivity(loginIntent);
+            }
+        });
     }
 }
