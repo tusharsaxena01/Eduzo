ccca37cdf23709e8b79a6c87939de0ffbf951c4f
Abhi Saxena <saxena.abhi7007@gmail.com>
2023-07-31 01:02:30 +0530

    added functionality and created NavigationActivity

--- app/src/main/java/com/bitAndroid/eduzo/Activities/NavigationActivity.java
+++ app/src/main/java/com/bitAndroid/eduzo/Activities/NavigationActivity.java
@@ -1,19 +1,17 @@
 package com.bitAndroid.eduzo.Activities;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
-import com.bitAndroid.eduzo.Classes.UserData;
 import com.bitAndroid.eduzo.Fragments.NavigationFragment;
 import com.bitAndroid.eduzo.R;
 import com.bitAndroid.eduzo.databinding.ActivityNavigationBinding;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+
+import java.util.Objects;
 
 public class NavigationActivity extends AppCompatActivity {
     ActivityNavigationBinding binding;
@@ -28,7 +26,8 @@ public class NavigationActivity extends AppCompatActivity {
         firebaseDatabase = FirebaseDatabase.getInstance();
         setContentView(binding.getRoot());
 
-        String uuid = firebaseAuth.getCurrentUser().getUid();
+        String uuid = Objects.requireNonNull(firebaseAuth.getCurrentUser()).getUid();
+        Log.w("uid", uuid);
 
 
         // Setting fragment container to change
