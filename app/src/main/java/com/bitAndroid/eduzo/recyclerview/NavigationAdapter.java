567fa880ed86887ca228c1458f1bbc7be709ce70
Abhi Saxena <saxena.abhi7007@gmail.com>
2023-08-01 01:51:09 +0530

    added functionality and created NavigationActivity

--- app/src/main/java/com/bitAndroid/eduzo/recyclerview/NavigationAdapter.java
+++ app/src/main/java/com/bitAndroid/eduzo/recyclerview/NavigationAdapter.java
@@ -2,17 +2,22 @@ package com.bitAndroid.eduzo.recyclerview;
 
 import static android.R.color.transparent;
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bitAndroid.eduzo.activities.WelcomeActivity;
 import com.bitAndroid.eduzo.databinding.NavigationRecyclerLayoutBinding;
 import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.ArrayList;
 
@@ -43,8 +48,31 @@ public class NavigationAdapter extends RecyclerView.Adapter<NavigationAdapter.Vi
         // Todo: Add transparent background
         holder.binding.cvMain.setBackgroundResource(transparent);
         // Todo: Add on click functions
+        // Todo: Add More as per need
+        holder.binding.getRoot().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkItemName(item);
+            }
+        });
+
 
     }
+
+    private void checkItemName(NavigationRecyclerData item) {
+        switch(item.itemText){
+            case "Logout":
+                Toast.makeText(context, item.itemText+" Clicked", Toast.LENGTH_SHORT).show();
+                // Todo: Move to a function
+                FirebaseAuth.getInstance().signOut();
+                Intent welcomeIntent = new Intent(context, WelcomeActivity.class);
+                context.startActivity(welcomeIntent);
+                break;
+            default:
+                break;
+        }
+    }
+
 
     @Override
     public int getItemCount() {
