2c68f5b99e19292dc6e599ae4f757b39b7143fb2
Abhi Saxena <saxena.abhi7007@gmail.com>
2023-08-04 21:41:59 +0530

    final commit before submittion

--- app/src/main/java/com/bitAndroid/eduzo/activities/WelcomeActivity.java
+++ app/src/main/java/com/bitAndroid/eduzo/activities/WelcomeActivity.java
@@ -10,7 +10,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Toast;
 
@@ -33,23 +32,17 @@ import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class WelcomeActivity extends AppCompatActivity {
-    private ActivityWelcomeBinding binding;
-    private SharedPreferences sp;
-    private FirebaseAuth firebaseAuth;
-    private GoogleSignInClient googleSignInClient;
-
-    private static final int RC_SIGN_IN = 1234;
-    private static final String DATA_PREFS_NAME = "data";
-    private static final String FIRST_VISIT_KEY = "first_visit";
-
+    ActivityWelcomeBinding binding;
+    SharedPreferences sp;
+    FirebaseAuth firebaseAuth;
+    GoogleSignInClient googleSignInClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityWelcomeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        sp = getSharedPreferences(DATA_PREFS_NAME, MODE_PRIVATE);
+        sp = getSharedPreferences("data", 0);
         firebaseAuth = FirebaseAuth.getInstance();
-
         GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
@@ -57,112 +50,174 @@ public class WelcomeActivity extends AppCompatActivity {
 
         googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
 
-        binding.ivBack.setOnClickListener(v -> {
-            startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
+
+        binding.ivBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent getStartedIntent = new Intent(WelcomeActivity.this, MainActivity.class);
+                startActivity(getStartedIntent);
+            }
         });
 
-        binding.btnGoogle.setOnClickListener(v -> {
-            Intent googleSignInIntent = googleSignInClient.getSignInIntent();
-            startActivityForResult(googleSignInIntent, RC_SIGN_IN);
+        binding.btnGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Todo: add login with google
+
+                // authentication code
+                Intent googleSignInIntent = googleSignInClient.getSignInIntent();
+                startActivityForResult(googleSignInIntent, 1234);
+
+                //after authentication
+
+                sp.edit()
+                        .putInt("first_visit", 1)
+                        .apply();
+
+            }
         });
 
-        binding.btnEmail.setOnClickListener(v -> {
-            Intent registerIntent = new Intent(WelcomeActivity.this, RegisterActivity.class);
-            startActivity(registerIntent);
+        binding.btnEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Todo: add login with email
+//                Intent loginActivityIntent = new Intent(WelcomeActivity.this, LoginActivity.class);
+                Intent registerIntent = new Intent(WelcomeActivity.this, RegisterActivity.class);
+                startActivity(registerIntent);
+            }
         });
 
-        binding.tvLogin.setOnClickListener(v -> {
-            Intent loginIntent = new Intent(WelcomeActivity.this, LoginActivity.class);
-            startActivity(loginIntent);
+        binding.tvLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Todo: add intent to move to login page
+                Intent loginIntent = new Intent(WelcomeActivity.this, LoginActivity.class);
+                startActivity(loginIntent);
+            }
         });
     }
 
+    // for google authentication
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == RC_SIGN_IN) {
+        if(requestCode == 1234) {
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
             try {
                 GoogleSignInAccount account = task.getResult(ApiException.class);
                 AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
                 firebaseAuth.signInWithCredential(credential)
-                        .addOnCompleteListener(task1 -> {
-                            if (task1.isSuccessful()) {
-                                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                                if (user != null) {
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Todo: create next Activity in place of 'LoginActivity' in below code (call 1) -- done
+                                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                                    assert user != null;
                                     String uuid = user.getUid();
                                     String name = user.getDisplayName();
                                     String email = user.getEmail();
                                     String mobileNo = user.getPhoneNumber();
-                                    String role = "Student"; // default role for google users
+                                    // default role for google users
+                                    String role = "Student";
                                     UserData googleUser = new UserData(uuid, name, mobileNo, email, role);
                                     saveUser(googleUser);
                                     Intent intent = new Intent(getApplicationContext(), NavigationActivity.class);
                                     startActivity(intent);
+                                } else {
+                                    try{
+                                        Toast.makeText(WelcomeActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                        showErrorDialog(task.getException().getMessage());
+                                    }catch (NullPointerException e){
+                                        showErrorDialog();
+                                    }
                                 }
-                            } else {
-                                showErrorDialog(task1.getException().getMessage());
                             }
                         });
             } catch (ApiException e) {
+//                throw new RuntimeException(e);
                 showErrorDialog();
             }
         }
+
     }
 
     public void showErrorDialog() {
-        showErrorDialog("An error occurred during authentication.");
+        ErrorDialogBinding errorDialogBinding = ErrorDialogBinding.inflate(getLayoutInflater());
+        AlertDialog dialog;
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(errorDialogBinding.getRoot());
+        dialog = builder.create();
+        errorDialogBinding.btnOkay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+        errorDialogBinding.cardMain.setBackgroundResource(transparent);
+        dialog.getWindow().setBackgroundDrawableResource(transparent);
     }
 
     public void showErrorDialog(String message) {
-        try {
-            ErrorDialogBinding errorDialogBinding = ErrorDialogBinding.inflate(getLayoutInflater());
-            errorDialogBinding.tvMessage.setText(message);
+        ErrorDialogBinding errorDialogBinding = ErrorDialogBinding.inflate(getLayoutInflater());
 
-            AlertDialog dialog;
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setView(errorDialogBinding.getRoot());
-            dialog = builder.create();
-            errorDialogBinding.btnOkay.setOnClickListener(v -> dialog.dismiss());
-            dialog.show();
-            errorDialogBinding.cardMain.setBackgroundResource(transparent);
-            dialog.getWindow().setBackgroundDrawableResource(transparent);
-        } catch (Exception e) {
-            Toast.makeText(getBaseContext(), "Firebase Connection Error", Toast.LENGTH_SHORT).show();
-            Toast.makeText(getBaseContext(), "Exiting Application", Toast.LENGTH_SHORT).show();
-            finishAffinity();
-        }
+        // change layout elements
+        errorDialogBinding.tvMessage.setText(message);
+
+        AlertDialog dialog;
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(errorDialogBinding.getRoot());
+        dialog = builder.create();
+        errorDialogBinding.btnOkay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+        errorDialogBinding.cardMain.setBackgroundResource(transparent);
+        dialog.getWindow().setBackgroundDrawableResource(transparent);
     }
+
 
     @Override
     protected void onStart() {
         super.onStart();
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        if (user != null) {
-            startActivity(new Intent(this, NavigationActivity.class));
+        if(user != null){
+            // Todo: create next Activity in place of 'LoginActivity' in below code (call 2) -- done
+
+            Intent intent = new Intent(this, NavigationActivity.class);
+            startActivity(intent);
         }
     }
 
     private void saveUser(UserData data) {
-        try {
+
+        try{
             String uuid = firebaseAuth.getUid();
             String role = "Student";
 
             binding.pbLoading.setVisibility(View.VISIBLE);
 
+            assert uuid != null;
             FirebaseDatabase.getInstance()
                     .getReference()
                     .child("Registered Users")
                     .child(uuid)
                     .setValue(data)
-                    .addOnCompleteListener(task -> {
-                        if (task.isSuccessful()) {
-                            Toast.makeText(WelcomeActivity.this, "User Saved Successfully", Toast.LENGTH_SHORT).show();
-                        } else {
-                            Toast.makeText(WelcomeActivity.this, "User Not Saved " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                            showErrorDialog();
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(WelcomeActivity.this, "User Saved Successfully", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(WelcomeActivity.this, "User Not Saved " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                showErrorDialog();
+                            }
+                            binding.pbLoading.setVisibility(View.GONE);
                         }
-                        binding.pbLoading.setVisibility(View.GONE);
                     });
         } catch (NullPointerException e) {
             showErrorDialog();
